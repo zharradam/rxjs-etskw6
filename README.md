7d7cbd3eaa2de19e21712ed609b55d75541e90ec
zharradam <zharradam@hotmail.com>
2021-06-21 20:14:10 +0930

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# rxjs-etskw6
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/rxjs-etskw6)
---
+++ index.ts
@@ -0,0 +1,6 @@
+import { of } from "rxjs";
+import { map } from "rxjs/operators";
+
+const source = of("World").pipe(map(x => `Hello ${x}!`));
+
+source.subscribe(console.log);
